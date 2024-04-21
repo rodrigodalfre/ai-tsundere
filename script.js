f19f3cb5a6505638db1d13be4277b8f931a1a10b
Rodrigo Dalfré <67020557+rodrigodalfre@users.noreply.github.com>
2024-04-21 18:53:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const uri = 'https://83bc-34-150-216-226.ngrok-free.app/';
+const uri = 'https://074c-34-106-84-254.ngrok-free.app/';
 const url = `${uri}ask`
 const question = 'What do you like to do in free time?';
 
